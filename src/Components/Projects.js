e913318fef209772ba6bc9a35edcd66002121301
saiakulas <saiakula79989@gmail.com>
2025-02-10 21:02:05 +0530

    changes

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -5,20 +5,34 @@ import cafe from '../images/cafe.png';
 import quiz from '../images/quiz.png';
 import finance from '../images/finance.png';
 import task from '../images/task.png';
+import freshnz from '../images/freshnz.png';
+import booksaver from '../images/booksaver.png';
 
 const Projects = () => {
   const projectData = [
+    {
+      projectPhoto: task,
+      projectName: "Personalized Task Remainder",
+      projectDesc: "A task management app with personalized reminders.",
+      projectGithub: "https://github.com/saiakulas/Task-Remainder.git"
+    },
+    {
+      projectPhoto: freshnz,
+      projectName: "Freshnz",
+      projectDesc: "An e-commerce platform to sell organic natural products, developed with React, Node.js, MongoDB, and Chakra UI.",
+      projectGithub: "https://github.com/saiakulas/Freshnz.git"
+    },
+    {
+      projectPhoto: booksaver,
+      projectName: "Digital Books Saver",
+      projectDesc: "A platform to save books and maintain a personal collection, built with Flask, MongoDB, and React.",
+      projectGithub: "https://github.com/saiakulas/Digital-Books-Saver.git"
+    },
     {
       projectPhoto: eco,
       projectName: "online shopping-redux",
       projectDesc: "An online shopping application built with Redux for state management.",
       projectGithub: "https://github.com/saiakulas/ecommerce-store.git"
-    },
-    {
-      projectPhoto: cafe,
-      projectName: "digital cafe info",
-      projectDesc: "A digital information system for cafes, providing menus and more.",
-      projectGithub: "https://github.com/saiakulas/coeffe-project.git"
     },
     {
       projectPhoto: quiz,
@@ -32,12 +46,7 @@ const Projects = () => {
       projectDesc: "A management system for vehicle financing.",
       projectGithub: "https://github.com/saiakulas/Finance-app.git"
     },
-    {
-      projectPhoto: task,
-      projectName: "Personalized Task Remainder",
-      projectDesc: "A task management app with personalized reminders.",
-      projectGithub: "https://github.com/saiakulas/Task-Remainder.git"
-    },
+
   ];
 
   return (
